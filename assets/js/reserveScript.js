8c4f1b4265cf3252f10a6d454aa7ff59d8ddb733
ht-nam <hoangthanhnam10@gmail.com>
2022-06-12 18:46:58 +0700

    Fix login reserve bug

--- assets/js/reserveScript.js
+++ assets/js/reserveScript.js
@@ -1,15 +1,20 @@
-// const vipBtn = document.getElementById("vip-btn");
-// vipBtn.onclick = function () {
-//     const d = document.getElementById("radio-vip");
-//     d.checked = true;
-// }
 window.onload = start;
 function start() {
+    // form param
     const vipBtn = document.getElementById("vip-btn");
     const doubleBtn = document.getElementById("double-btn");
     const singleBtn = document.getElementById("single-btn");
     const affordableBtn = document.getElementById("affordable-btn");
     
+    // login modal param
+    const loginBtn = document.getElementById("lgBtn");
+    const rgChangeBtn = document.getElementById("rgChange");
+    const lgChagneBtn = document.getElementById("lgChange");
+    const modalCloses = document.querySelectorAll(".fa-xmark");
+    const modalForgets = document.querySelectorAll(".ForgetPassword");
+
+
+    // form func
     vipBtn.onclick = function () {
         const d = document.getElementById("radio-vip");
         d.checked = true;
@@ -25,6 +30,31 @@ function start() {
     affordableBtn.onclick = function () {
         const d = document.getElementById("radio-affordable");
         d.checked = true;
+    }
+
+    // login func
+    loginBtn.onclick = function () {
+        document.getElementById("modal").style.display = "flex";
+    }
+    rgChangeBtn.onclick = function () {
+        document.querySelector(".modal-login").style.display = "none";
+        document.querySelector(".modal-register").style.display = "block";
+    }
+    lgChagneBtn.onclick = function () {
+        document.querySelector(".modal-register").style.display = "none";
+        document.querySelector(".modal-login").style.display = "block";
+    }
+
+    for (const modalClose of modalCloses) {
+        modalClose.onclick = function () {
+            document.getElementById("modal").style.display = "none";
+        }
+    }
+
+    for (const modalForget of modalForgets) {
+        modalForget.onclick = function () {
+            alert("Please contact the administrator for more information")
+        }
     }
 }
 
